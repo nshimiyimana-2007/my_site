5eb2642f1515ba29704f31f97943c7c1c4587955
nshimiyimana-2007 <nshimiyimanaarsene4@gmail.com>
2026-01-17 14:51:47 +0200

    Add files via upload

---
+++ scriot.js
@@ -0,0 +1,8 @@
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({behavior: "smooth"});
+}
+
+function sendMessage(event) {
+    event.preventDefault();
+    alert("Murakoze! Ubutumwa bwanyu bwakiriwe ❤️");
+}
